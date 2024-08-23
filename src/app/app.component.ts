f05b38c9d0c2d731bcd3f86b5e2d41b892fd0be1
Trouthe <anthony.hajjar00@gmail.com>
2024-08-23 15:19:22 +0300

    Animation Arep

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,25 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  HostListener,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ViewportScroller } from '@angular/common';
 
 import { ThemeService } from './services/theme.service';
-
 import { NavbarDesktopComponent } from './components/navbar-desktop/navbar-desktop.component';
 import { NavbarMobileComponent } from './components/navbar-mobile/navbar-mobile.component';
 import { EducationComponent } from './components/education/education.component';
 import { ProfessionalSkillsComponent } from './components/professional-skills/professional-skills.component';
 import { WorkComponent } from './components/work/work.component';
 import { PortfolioComponent } from './components/portfolio/portfolio.component';
-import { ViewportScroller } from '@angular/common';
+
+// Smoothing and Animations
+import Lenis from 'lenis';
+import gsap from 'gsap';
+import { Observer } from 'gsap/all';
 
 @Component({
   selector: 'app-root',
@@ -26,7 +36,7 @@ import { ViewportScroller } from '@angular/common';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit, OnDestroy {
   title = 'portfolio-website';
 
   isMobile: boolean = false;
@@ -36,6 +46,11 @@ export class AppComponent implements OnInit {
   githubIcon!: string;
   linkedinIcon!: string;
   mailIcon!: string;
+
+  scrollSmooth: Lenis = new Lenis({
+    duration: 0.85,
+    syncTouch: true,
+  });
 
   constructor(
     private theme: ThemeService,
@@ -54,6 +69,62 @@ export class AppComponent implements OnInit {
     this.githubIcon = this.theme.getGithubIcon();
     this.linkedinIcon = this.theme.getLinkedinIcon();
     this.mailIcon = this.theme.getMailIcon();
+
+    this.smoothScroll();
+
+    gsap.registerPlugin(Observer);
+
+    gsap.fromTo(
+      '.header-item',
+      { y: 25, opacity: 0 },
+      { y: 0, opacity: 1, ease: 'power4.out', stagger: 0.1, duration: 1.2 }
+    );
+    gsap.fromTo(
+      '.about-item',
+      { y: 25, opacity: 0 },
+      { y: 0, opacity: 1, ease: 'power4.out', stagger: 0.15, duration: 1 }
+    );
+
+    const aboutItems = gsap.utils.toArray('.about-item') as HTMLElement[];
+
+    aboutItems.forEach((item: HTMLElement) => {
+      Observer.create({
+        target: item, // Attach the observer to each item individually
+        type: 'pointer', // Handle pointer events
+        onHover: () => {
+          gsap.to(item, {
+            // Math.random() * (max - min): Scales the random number to be within the range of 0 to (max - min).
+            // + min: Shifts the scaled number to the desired range [min, max].
+            rotateZ: Math.random() * (-0.25 - 0.5) + 0.5,
+            duration: 0.25,
+          });
+        },
+        onHoverEnd: () => {
+          gsap.to(item, {
+            rotateZ: 0,
+            duration: 0.25,
+          });
+        },
+      });
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.scrollSmooth.stop();
+  }
+
+  smoothScroll(): void {
+    // Debugging
+    // this.lenis.on('scroll', (e: any) => {
+    //   console.log(e);
+    // });
+
+    const raf = (time: number) => {
+      this.scrollSmooth.raf(time);
+      requestAnimationFrame(raf);
+    };
+
+    requestAnimationFrame(raf);
   }
 
   checkWindow(): void {
